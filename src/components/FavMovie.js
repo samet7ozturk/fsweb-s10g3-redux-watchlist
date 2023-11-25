81a4412be0f36c05c97aab7dd8f3030966dbb223
samet7ozturk <sametozturk973@gmail.com>
2023-11-25 15:44:14 +0300

    Create func removeFromFav

--- src/components/FavMovie.js
+++ src/components/FavMovie.js
@@ -1,8 +1,23 @@
-export default function FavMovie({ title, id }) {
+import { REMOVE_FAV } from "../store/actions/favoritesAction";
+import { RECYCLE_MOV } from "../store/actions/movieAction";
+import { useDispatch } from "react-redux";
+
+export default function FavMovie(props) {
+  const { id, title } = props.movie;
+
+  const dispatch = useDispatch();
+  const removeFromFav = () => {
+    dispatch({ type: REMOVE_FAV, payload: id });
+    dispatch({ type: RECYCLE_MOV, payload: props.movie });
+  };
+
   return (
     <div className="flex p-3 pl-4 bg-white mb-2 shadow items-center group">
       <div className="pr-4 flex-1">{title}</div>
-      <button className="px-3 py-2 block text-sm rounded bg-rose-700 text-white opacity-30 group-hover:opacity-100">
+      <button
+        onClick={removeFromFav}
+        className="px-3 py-2 block text-sm rounded bg-rose-700 text-white opacity-30 group-hover:opacity-100"
+      >
         Çıkar
       </button>
     </div>
