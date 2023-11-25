132f9bfdc5cd9a871b93df6442fe591ae41579fc
samet7ozturk <sametozturk973@gmail.com>
2023-11-25 14:26:27 +0300

    useSelector add

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { Switch, Route, NavLink } from "react-router-dom";
 import Movie from "./components/Movie";
 import FavMovie from "./components/FavMovie";
 
+import { UseSelector, useSelector } from "react-redux";
+
 function App() {
   const [sira, setSira] = useState(0);
-  const favMovies = [];
+  const favMovies = useSelector((state) => state.favoritesReducer.favMovies);
 
   function sonrakiFilm() {
     setSira(sira + 1);
@@ -14,10 +16,19 @@ function App() {
   return (
     <div className="wrapper max-w-2xl mx-auto">
       <nav className="flex text-2xl pb-6 pt-8 gap-2 justify-center">
-        <NavLink to="/" exact className="py-3 px-6 " activeClassName="bg-white shadow-sm text-blue-600">
+        <NavLink
+          to="/"
+          exact
+          className="py-3 px-6 "
+          activeClassName="bg-white shadow-sm text-blue-600"
+        >
           Filmler
         </NavLink>
-        <NavLink to="/listem" className="py-3 px-6 " activeClassName="bg-white shadow-sm text-blue-600">
+        <NavLink
+          to="/listem"
+          className="py-3 px-6 "
+          activeClassName="bg-white shadow-sm text-blue-600"
+        >
           Listem
         </NavLink>
       </nav>
