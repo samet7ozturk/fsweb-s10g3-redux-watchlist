e43122ea0e45769d8f3b291451c4e8eae540486d
samet7ozturk <sametozturk973@gmail.com>
2023-11-25 13:53:26 +0300

    combineReducers

--- src/store/index.js
+++ src/store/index.js
@@ -5,3 +5,15 @@ import {
 } from "redux";
 
 import logger from "redux-logger";
+
+import moviesReducer from "./reducers/moviesReducer";
+import favoritesReducer from "./reducers/favoritesReducer";
+
+const rootReducer = combineReducers({
+  moviesReducer,
+  favoritesReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+export default store;
